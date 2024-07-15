1c70f77aa812e5c46713a4f7a3960f4bfcff0c7d
liguangliang <13537223157@163.com>
2024-07-15 15:28:46 +0800

    feat: tdd 实现库功能

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use std::{env, fs};
-
+use std::{env,  process};
+use cli::Config;
+use cli::run;
 fn main() {
-    let args:Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {}",query);
-    println!("In file {}",file_path);
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    println!("with text:\n{contents}")
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments:{err}");
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    if let Err(e) = run(config){
+        println!("Application error:{e}");
+        process::exit(1);
+    }
 }
+
+
+
